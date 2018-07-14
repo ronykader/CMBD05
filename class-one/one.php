685ca6e2279a5725ec59671211b088215f17940e
Rony Kader <rony.ksr06@gmail.com>
2018-07-14 06:37:08 +0600

    update

---
+++ class-one/one.php
@@ -0,0 +1,24 @@
+<?php 
+ echo "amra kiso dakhate cai"; 
+
+
+// echo "amra kiso dakhate cai"; 
+
+# echo "r ek vabe comment"
+
+
+/*echo "amra kiso dakhate cai";
+echo "amra kiso dakhate cai";
+echo "amra kiso dakhate cai";*/
+
+
+$rony = "Rony";
+$rony = "amar nam rony";
+
+
+$name = "kiso data raklam aer modhokiso data raklam aer modhokiso data raklam aer modhokiso data raklam aer modhokiso data raklam aer modhokiso data raklam aer modho";
+
+echo "<br>";
+echo $rony;
+
+?>
